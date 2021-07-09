c8fa144d6cbcd6a53887d368504a183e18f8f1ae
eshahaldar <87066363+eshahaldar@users.noreply.github.com>
2021-07-09 17:48:12 +0530

    DAY 3 Assignment 2 (string palindrome)

---
+++ Assignment2(string palindrome).js
@@ -0,0 +1,13 @@
+let str = "My madam is talking in malayalam "
+let str1 = "", str2 = ""
+  for (t of str) {
+    if (t != " ") {
+      str2 = str2 + t
+      str1 = t + str1
+    }
+    else {
+      if (str1 == str2) console.log(str1)
+      str1 = ""
+      str2 = ""
+    }
+  }
