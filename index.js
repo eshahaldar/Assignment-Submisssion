e953a250632467fa5451cf68cb6e5a4975b30248
eshahaldar <87066363+eshahaldar@users.noreply.github.com>
2021-07-12 18:56:00 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function doSomething() {
-    var inp = document.getElementById("txt-in").value;
-    // console.log(inp);
-    document.getElementById("head").innerHTML = "Hello " + inp;
-}
-
-var btn = document.getElementById("btn");
-btn.addEventListener("click", doSomething);
